63ef4abf8776e668b015e7590bd470705a503ef1
gamedevshirious <sad3119823@gmail.com>
2021-01-27 10:07:49 +0530

    Daily backup Wed, Jan 27, 2021 10:07:49 AM

--- game_scenes/MainMenu.gd
+++ game_scenes/MainMenu.gd
@@ -5,12 +5,23 @@ var hero_index = 0
 
 var dir
 
-func _process(_delta):
-	pass
+
+var meshes = [
+	"Mesh/head/head",
+	"Mesh/torso/torso",
+	"Mesh/torso/left/upper",
+	"Mesh/torso/left/upper/elbow/hand",
+	"Mesh/torso/right/upper",
+	"Mesh/torso/right/upper/elbow/hand",
+	"Mesh/legs/right/upper",
+	"Mesh/legs/right/upper/knee/lower",
+	"Mesh/legs/left/upper",
+	"Mesh/legs/left/upper/knee/lower"
+]
 
 func _ready():
 	globals.player_name = globals.save_game["player_name"]
-	$TypeSelector/PlayerNameLabel.text = globals.player_name
+	$PlayerNameLabel.text = globals.player_name
 
 	dir = Directory.new()
 	load_shape()
@@ -27,28 +38,28 @@ func load_shape():
 			heroes.append(file)
 
 	dir.list_dir_end()
-	load_curr_hero()
-#	for i in $TypeSelector/heros.get_item_count():
-#		$TypeSelector/heros.remove_item(0)
+	# load_curr_hero()
+#	for i in $heros.get_item_count():
+#		$heros.remove_item(0)
 #
 #	for i in heroes:
-#		$TypeSelector/heros.add_item(i)
-func load_curr_hero():
-	$TypeSelector/sprite.texture = load("res://characters/"+heroes[hero_index]+"/head.png")
+#		$heros.add_item(i)
+#func load_curr_hero():
+#	$sprite.texture = load("res://characters/.head.png")
 
 func _on_SinglePlayer_pressed():
 	globals.curr_hero = heroes[hero_index]
 	globals.mode = "singleplayer"
-	globals.player_name = $TypeSelector/PlayerNameLabel.text
+	globals.player_name = $PlayerNameLabel.text
 	get_tree().change_scene("res://game_scenes/levels/Dojo.tscn")
-
-func _on_Prev_pressed():
-	hero_index = (hero_index - 1) % heroes.size()
-	load_curr_hero()
-
-func _on_Next_pressed():
-	hero_index = (hero_index - 1) % heroes.size()
-	load_curr_hero()
+#
+#func _on_Prev_pressed():
+#	hero_index = (hero_index - 1) % heroes.size()
+##	load_curr_hero()
+#
+#func _on_Next_pressed():
+#	hero_index = (hero_index - 1) % heroes.size()
+#	load_curr_hero()
 
 func _on_Multiplayer_pressed():
 	globals.curr_hero = heroes[hero_index]
@@ -65,6 +76,10 @@ func _on_LineEdit_text_changed(new_text):
 
 
 func _on_ShadeColorPicker_color_changed(color):
-	$TypeSelector/sprite.modulate = color
+#	$sprite.modulate = color
 	globals.color = var2str(color)
-
+	
+	var material = $player/Mesh/head/head.get_surface_material(0).duplicate()
+	material.set_shader_param("base_color", color)
+	for node in meshes:
+		get_node("player/" + node).set_surface_material(0, material)
