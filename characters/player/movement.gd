d75f46191e661235e33d8e9169876a04aeea1db9
gamedevshirious <sad3119823@gmail.com>
2021-02-08 03:21:28 +0530

    Daily backup Mon, Feb  8, 2021  3:21:27 AM

--- characters/player/movement.gd
+++ characters/player/movement.gd
@@ -24,25 +24,27 @@ func move(_self):
 	move_vec.x -= Input.get_action_strength("ui_left")
 
 
-#	if abs(move_vec.z) > 1:
-#		$AnimationTree.set("parameters/walk/add_amount",  move_vec.z/10)
-#	else:
-#		$AnimationTree.set("parameters/walk/add_amount",  0)
+	var grounded = _self.is_on_floor()
 
 	move_vec = move_vec.normalized()
 	move_vec = move_vec.rotated(Vector3(0, 1, 0), _self.rotation.y)
 	move_vec *= MOVE_SPEED
 	move_vec.y = y_velo
-
-
-
+	
+	_self.get_node("Mesh/legs").rotation.y = move_vec.rotated(Vector3(0, 1, 0), _self.rotation.y).y
+	
 	move_vec = _self.move_and_slide(move_vec, Vector3(0, 1, 0))
 
-	var grounded = _self.is_on_floor()
+	if abs(move_vec.z) > 0.1:
+		if grounded:
+			_self.get_node("AnimationTree").set("parameters/walk/add_amount",  1)
+	else:
+		_self.get_node("AnimationTree").set("parameters/walk/add_amount",  0)
+
+	
 	y_velo -= GRAVITY
-	var just_jumped = false
+	
 	if grounded and Input.is_action_just_pressed("jump"):
-		just_jumped = true
 		y_velo = JUMP_FORCE
 
 	if grounded and y_velo <= 0:
@@ -56,5 +58,5 @@ func move(_self):
 				_self.cam.translation = Vector3(3, 4, 4)
 				_self.zoomed_in = true
 			else:
-				_self.cam.translation = Vector3(0, 4, 8)
+				_self.cam.translation = Vector3(0, 4, 5)
 				_self.zoomed_in = false
