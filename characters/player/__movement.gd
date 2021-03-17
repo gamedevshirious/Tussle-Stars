ba38354e397cd6d50961fcb467f0072124939939
gamedevshirious <sad3119823@gmail.com>
2021-03-17 10:20:45 +0530

    Daily backup Wed, Mar 17, 2021 10:20:44 AM

---
+++ characters/player/__movement.gd
@@ -0,0 +1,95 @@
+extends Node
+
+const MOVE_SPEED = 12
+const JUMP_FORCE = 50
+const GRAVITY = 0.98
+const MAX_FALL_SPEED = 30
+
+var y_velo = 0
+var current_facing = 0
+var mouseDelta
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouseDelta = event.relative
+		
+func move(delta):
+	var _self = get_parent().get_parent()
+	var move_vec = Vector3()
+	
+#
+#	if Input.is_action_pressed("ui_left"):
+##		move_vec.z -= 1
+#		_self.rotate_object_local(Vector3(0, 1, 0), .1)
+##		_self.get_node("Mesh").rotation_degrees.y = clamp(_self.get_node("Mesh").rotation_degrees.y, current_facing, 90)
+#	if Input.is_action_pressed("ui_right"):
+##		move_vec.z += 1
+#		_self.rotate_object_local(Vector3(0, 1, 0), -.1)
+		
+#		_self.get_node("Mesh").rotation_degrees.y = clamp(_self.get_node("Mesh").rotation_degrees.y, -90, current_facing)
+	if Input.is_action_pressed("ui_up"):
+		move_vec.x -= 1
+#		_self.translate_object_local(_self.transform.basis.x)
+#		_self.get_node("Mesh").rotation.y = lerp_angle(_self.get_node("Mesh").rotation.y, current_facing, 0.1)
+#		if _self.get_node("Mesh").rotation_degrees.y < 0:
+#			_self.get_node("Mesh").rotation_degrees.y += 5
+#			_self.get_node("Mesh").rotation_degrees.y = clamp(_self.get_node("Mesh").rotation_degrees.y, 0, current_facing)
+#		else:
+#			_self.get_node("Mesh").rotation_degrees.y -= 5
+#			_self.get_node("Mesh").rotation_degrees.y = clamp(_self.get_node("Mesh").rotation_degrees.y, current_facing, 0)
+
+			
+		
+#		_self.get_node("Mesh").rotation_degrees.y = lerp_angle(90, _self.rotation_degrees.y, .01)
+	if Input.is_action_pressed("ui_down"):
+		move_vec.x += 1
+#		_self.translate_object_local(-_self.transform.basis.x)
+	# _self.get_node("Mesh").rotation_degrees.y = lerp_angle(_self.get_node("Mesh").rotation_degrees.y, 0, .1)
+#	if _self.cam.name == "TPCamera":
+#	_self.get_node("Mesh").rotation.y = lerp_angle(_self.get_node("Mesh").rotation.y , 0, 45 * delta * .1)
+#	else:
+#		_self.rotation_degrees.y = _self.get_node("Mesh").rotation_degrees.y
+#		_self.get_node("Mesh").rotation_degrees.y = 0
+	current_facing = _self.get_node("TPCamera").rotation.y
+#	move_vec.z -= Input.get_action_strength("ui_up")
+#	move_vec.z += Input.get_action_strength("ui_down") / 2 
+#	move_vec.x += Input.get_action_strength("ui_right")
+#	move_vec.x -= Input.get_action_strength("ui_left")
+	_self.rotation.z = _self.get_node("TPCamera").rotatio
+
+	var grounded = _self.is_on_floor()
+
+	move_vec = move_vec.normalized()
+	move_vec = move_vec.rotated(Vector3(0, 1, 0), current_facing)
+	move_vec *= MOVE_SPEED
+	move_vec.y = y_velo
+	
+#	_self.get_node("Mesh/legs").rotation.y = move_vec.rotated(Vector3(0, 1, 0), _self.rotation.y).y
+	
+	move_vec = _self.move_and_slide(move_vec, Vector3(0, 1, 0))
+
+	if abs(move_vec.length_squared()) > 1:
+		if grounded:
+			_self.get_node("AnimationTree").set("parameters/walk/add_amount",  1)
+	else:
+		_self.get_node("AnimationTree").set("parameters/walk/add_amount",  0)
+
+	
+	y_velo -= GRAVITY
+	
+	if grounded and Input.is_action_just_pressed("jump"):
+		y_velo = JUMP_FORCE
+
+	if grounded and y_velo <= 0:
+		y_velo = -0.1
+	if y_velo < -MAX_FALL_SPEED:
+		y_velo = -MAX_FALL_SPEED
+
+#	if Input.is_action_just_pressed("focus"):
+#		if _self.cam.name == "TPCamera":
+#			if not _self.zoomed_in:
+#				_self.cam.translation = Vector3(4, 4, 3)
+#				_self.zoomed_in = true
+#			else:
+#				_self.cam.translation = Vector3(-5, 4, 0)
+#				_self.zoomed_in = false
--- characters/player/movement.gd
+++ characters/player/movement.gd
@@ -1,95 +1,59 @@
 extends Node
 
+var lookSensitivity : float = 15.0
+var minLookAngle : float = -20.0
+var maxLookAngle : float = 75.0
+ 
+var mouseDelta : Vector2 = Vector2()
+
 const MOVE_SPEED = 12
-const JUMP_FORCE = 50
+const JUMP_FORCE = 55
 const GRAVITY = 0.98
 const MAX_FALL_SPEED = 30
 
+const H_LOOK_SENS = 1.0
+const V_LOOK_SENS = 1.0
+
 var y_velo = 0
-var current_facing = 0
-var mouseDelta
+var _self 
+var mesh
 
-func _input(event):
-	if event is InputEventMouseMotion:
-		mouseDelta = event.relative
-		
+func _init():
+	_self = get_parent().get_parent()
+	mesh = _self.get_node("Mesh")
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func move(delta):
-	var _self = get_parent().get_parent()
-	var move_vec = Vector3()
+	var screen_pos = get_viewport().get_camera().unproject_position(_self.global_transform.origin)
+	var mouse_pos = get_viewport().get_mouse_position()
+	var angle = screen_pos.angle_to_point(mouse_pos)
+#	_self.rotation.y = -angle 
 	
-#
-#	if Input.is_action_pressed("ui_left"):
-##		move_vec.z -= 1
-#		_self.rotate_object_local(Vector3(0, 1, 0), .1)
-##		_self.get_node("Mesh").rotation_degrees.y = clamp(_self.get_node("Mesh").rotation_degrees.y, current_facing, 90)
-#	if Input.is_action_pressed("ui_right"):
-##		move_vec.z += 1
-#		_self.rotate_object_local(Vector3(0, 1, 0), -.1)
-		
-#		_self.get_node("Mesh").rotation_degrees.y = clamp(_self.get_node("Mesh").rotation_degrees.y, -90, current_facing)
+	var move_vec = Vector3()
 	if Input.is_action_pressed("ui_up"):
-		move_vec.x -= 1
-#		_self.translate_object_local(_self.transform.basis.x)
-#		_self.get_node("Mesh").rotation.y = lerp_angle(_self.get_node("Mesh").rotation.y, current_facing, 0.1)
-#		if _self.get_node("Mesh").rotation_degrees.y < 0:
-#			_self.get_node("Mesh").rotation_degrees.y += 5
-#			_self.get_node("Mesh").rotation_degrees.y = clamp(_self.get_node("Mesh").rotation_degrees.y, 0, current_facing)
-#		else:
-#			_self.get_node("Mesh").rotation_degrees.y -= 5
-#			_self.get_node("Mesh").rotation_degrees.y = clamp(_self.get_node("Mesh").rotation_degrees.y, current_facing, 0)
-
-			
-		
-#		_self.get_node("Mesh").rotation_degrees.y = lerp_angle(90, _self.rotation_degrees.y, .01)
+		move_vec.z -= 1
+		_self.rotation.y = lerp_angle(_self.rotation.y, _self.cam.rotation.y, 0.1)
 	if Input.is_action_pressed("ui_down"):
+		move_vec.z += 1
+	if Input.is_action_pressed("ui_right"):
 		move_vec.x += 1
-#		_self.translate_object_local(-_self.transform.basis.x)
-	# _self.get_node("Mesh").rotation_degrees.y = lerp_angle(_self.get_node("Mesh").rotation_degrees.y, 0, .1)
-#	if _self.cam.name == "TPCamera":
-#	_self.get_node("Mesh").rotation.y = lerp_angle(_self.get_node("Mesh").rotation.y , 0, 45 * delta * .1)
-#	else:
-#		_self.rotation_degrees.y = _self.get_node("Mesh").rotation_degrees.y
-#		_self.get_node("Mesh").rotation_degrees.y = 0
-	current_facing = _self.get_node("TPCamera").rotation.y
-#	move_vec.z -= Input.get_action_strength("ui_up")
-#	move_vec.z += Input.get_action_strength("ui_down") / 2 
-#	move_vec.x += Input.get_action_strength("ui_right")
-#	move_vec.x -= Input.get_action_strength("ui_left")
-	_self.rotation.z = _self.get_node("TPCamera").rotatio
-
-	var grounded = _self.is_on_floor()
-
+	if Input.is_action_pressed("ui_left"):
+		move_vec.x -= 1
+		
 	move_vec = move_vec.normalized()
-	move_vec = move_vec.rotated(Vector3(0, 1, 0), current_facing)
+	move_vec = move_vec.rotated(Vector3(0, 1, 0), _self.rotation.y)
 	move_vec *= MOVE_SPEED
 	move_vec.y = y_velo
+	_self.move_and_slide(move_vec, Vector3(0, 1, 0))
 	
-#	_self.get_node("Mesh/legs").rotation.y = move_vec.rotated(Vector3(0, 1, 0), _self.rotation.y).y
-	
-	move_vec = _self.move_and_slide(move_vec, Vector3(0, 1, 0))
-
-	if abs(move_vec.length_squared()) > 1:
-		if grounded:
-			_self.get_node("AnimationTree").set("parameters/walk/add_amount",  1)
-	else:
-		_self.get_node("AnimationTree").set("parameters/walk/add_amount",  0)
-
-	
+	var grounded = _self.is_on_floor()
 	y_velo -= GRAVITY
-	
-	if grounded and Input.is_action_just_pressed("jump"):
+	var just_jumped = false
+	if grounded and Input.is_action_just_pressed("ui_accept"):
+		just_jumped = true
 		y_velo = JUMP_FORCE
-
 	if grounded and y_velo <= 0:
 		y_velo = -0.1
 	if y_velo < -MAX_FALL_SPEED:
 		y_velo = -MAX_FALL_SPEED
-
-#	if Input.is_action_just_pressed("focus"):
-#		if _self.cam.name == "TPCamera":
-#			if not _self.zoomed_in:
-#				_self.cam.translation = Vector3(4, 4, 3)
-#				_self.zoomed_in = true
-#			else:
-#				_self.cam.translation = Vector3(-5, 4, 0)
-#				_self.zoomed_in = false
+	
